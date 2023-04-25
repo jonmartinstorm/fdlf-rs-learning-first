905c8da6ee5f232f50a5626f76ff339d6b82511d
Jon-Martin Storm <jonmartin.storm@gmail.com>
2023-04-25 09:54:11 +0200

    Implemented BusType and Bus.

--- src/lib/power_system.rs
+++ src/lib/power_system.rs
@@ -1,9 +1,21 @@
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum BusType {
-    // ...
+    Slack,
+    PV,
+    PQ,
 }
 
+#[derive(Debug, Clone)]
 pub struct Bus {
-    // ...
+    pub id: usize,
+    pub name: String,
+    pub voltage: f64,
+    pub angle: f64,
+    pub p_gen: f64,
+    pub q_gen: f64,
+    pub p_load: f64,
+    pub q_load: f64,
+    pub bus_type: BusType,
 }
 
 pub struct Branch {
---
+++ tests/power_system_tests.rs
@@ -0,0 +1,49 @@
+use fdlf_rs_tutorial::power_system::{Bus, Branch, BusType, PowerSystem};
+
+#[test]
+fn test_bus() {
+    let bus1 =  Bus {
+        id: 1,
+        name: "Bus 1".to_string(),
+        voltage: 1.0,
+        angle: 0.0,
+        p_gen: 1.0,
+        q_gen: 0.0,
+        p_load: 0.0,
+        q_load: 0.0,
+        bus_type: BusType::Slack,
+    };
+
+    let bus2 =  Bus {
+        id: 2,
+        name: "Bus 2".to_string(),
+        voltage: 1.0,
+        angle: 0.0,
+        p_gen: -1.0, // negative generation is invalid
+        q_gen: 0.0,
+        p_load: 1.0,
+        q_load: 0.0,
+        bus_type: BusType::PQ,
+    };
+
+    assert_eq!(bus1.id, 1);
+    assert_eq!(bus1.name, "Bus 1");
+    assert_eq!(bus1.voltage, 1.0);
+    assert_eq!(bus1.angle, 0.0);
+    assert_eq!(bus1.p_gen, 1.0);
+    assert_eq!(bus1.q_gen, 0.0);
+    assert_eq!(bus1.p_load, 0.0);
+    assert_eq!(bus1.q_load, 0.0);
+    assert_eq!(bus1.bus_type, BusType::Slack);
+
+    assert_eq!(bus2.id, 2);
+    assert_eq!(bus2.name, "Bus 2");
+    assert_eq!(bus2.voltage, 1.0);
+    assert_eq!(bus2.angle, 0.0);
+    assert_eq!(bus2.p_gen, -1.0);
+    assert_eq!(bus2.q_gen, 0.0);
+    assert_eq!(bus2.p_load, 1.0);
+    assert_eq!(bus2.q_load, 0.0);
+    assert_eq!(bus2.bus_type, BusType::PQ);
+
+}
